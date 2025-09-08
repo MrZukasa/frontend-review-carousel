f80ff899d54744d1ac99fd3b64b5d38d43454fd1
Francesco Boasso <fra.boasso@gmail.com>
2025-09-08 15:59:42 +0200

    :construction: WIP per nuovo layout

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,21 +1,18 @@
-import { Link } from "react-router-dom";
 import { GameCardProps } from "../interfaces";
 
 const GameCard: React.FC<GameCardProps> = ({ game }: GameCardProps): React.ReactElement => {
   return (
     <div className="bg-gray-800 rounded-2xl shadow-lg overflow-hidden p-4 hover:bg-gray-700 transition">
-      <Link to={`/game/${game.id}`}>
-        <p className="text-xl font-semibold">{game.nomeGioco}</p>
-        <p className="text-sm text-gray-300">
-          Voto al lancio: {game.votoLancio ?? "N/D"}
-        </p>
-        <p className="text-sm text-gray-300">
-          Voto aggiornato: {game.votoAggiornato ?? "N/D"}
-        </p>
-        <p className="text-sm text-gray-400">
-          Ultima revisione: {game.ultimaRevisione || "N/D"}
-        </p>
-      </Link>
+      <p className="text-xl font-semibold">{game.nomeGioco}</p>
+      <p className="text-sm text-gray-300">
+        Voto al lancio: {game.votoLancio ?? "N/D"}
+      </p>
+      <p className="text-sm text-gray-300">
+        Voto aggiornato: {game.votoAggiornato ?? "N/D"}
+      </p>
+      <p className="text-sm text-gray-400">
+        Ultima revisione: {game.ultimaRevisione || "N/D"}
+      </p>
     </div>
   );
 };
